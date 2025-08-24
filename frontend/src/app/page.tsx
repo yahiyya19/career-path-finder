19c5f8c7c76ca827e2b3b16eb6af45e7043ff806
yahiyya19 <shainazsayyed13@gmail.com>
2025-08-24 12:50:40 +0530

    project

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,4 +9,16 @@ export default function Home(){
     </div>
     </>
   )
-}
+}<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Career Path Finder Landing Page Platform</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
+  </body>
+</html>
